e8eab762704434f4d2f23e0a9dc28fd2b6321b77
Chis <chitrankhon@gmail.com>
2026-03-10 22:37:54 +0700

    Fix ui flow error

--- mobile/app/src/main/java/com/example/travelplanning/ui/mainscreen/HomeFragment.java
+++ mobile/app/src/main/java/com/example/travelplanning/ui/mainscreen/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.travelplanning.ui.home;
+package com.example.travelplanning.ui.mainscreen;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
--- mobile/app/src/main/java/com/example/travelplanning/ui/mainscreen/MainScreenActivity.java
+++ mobile/app/src/main/java/com/example/travelplanning/ui/mainscreen/MainScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.travelplanning.ui.mainscreen;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
