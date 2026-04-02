59ac8d7a6eedb8196e5fb5c797d113a3f9b3f44f
GitGit190 <giaobao2kk5@gmail.com>
2026-04-02 09:02:41 +0700

    location manager's filter

--- server/src/routes/admin/index.routes.ts
+++ server/src/routes/admin/index.routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import userManageRoute from "./user_manage.routes";
 import locationManageRoute from "./location_manage.routes";
+import statRoute from "./stat.route";
+import reportRoute from "./report.routes";
 import { requireRole } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
@@ -9,5 +11,7 @@ const router = express.Router();
 router.use(requireRole("admin"));
 router.use("/user", userManageRoute);
 router.use("/location", locationManageRoute);
+router.use("/stats", statRoute);
+router.use("/reports", reportRoute);
 
 export default router;
